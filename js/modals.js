11c001836182cf1eb889df2982b18fd6c4785132
Dmitriy Yudichev <dimarik34rus@ya.ru>
2023-06-28 22:23:56 +0300

    update

--- js/modals.js
+++ js/modals.js
@@ -96,38 +96,3 @@
 		body.style.overflow = '';
 		body.style.paddingRight = 0;
 	});
-
-/* ------------------- Появление 2-й группы радиокнопок в регистации -------------------- */
-const genderRadios = document.getElementsByName('gender');
-const lookingRadios = document.getElementsByName('looking');
-const lookingGroups = document.querySelectorAll('.select-gender__group');
-
-// функция для проверки выбранного радио-элемента
-function isChecked(radios) {
-  for (let i = 0; i < radios.length; i++) {
-    if (radios[i].checked) {
-      return true;
-    }
-  }
-  return false;
-}
-
-// обработчик события изменения состояния радио-кнопок
-function handleRadioChange() {
-  for (let i = 0; i < lookingGroups.length; i++) {
-    if (isChecked(genderRadios)) {
-      lookingGroups[i].style.display = 'grid';
-    } else {
-      lookingGroups[i].style.display = 'none';
-    }
-  }
-}
-
-// добавляем обработчик на каждую группу радио-кнопок
-for (let i = 0; i < genderRadios.length; i++) {
-  genderRadios[i].addEventListener('change', handleRadioChange);
-}
-
-for (let i = 0; i < lookingRadios.length; i++) {
-  lookingRadios[i].addEventListener('change', handleRadioChange);
-}
